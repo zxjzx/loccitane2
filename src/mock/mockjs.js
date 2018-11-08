1238af194217fe3a29bae9a116a1a6983af0faab
jane <jane@interbiztech.cn>
2018-11-08 15:24:09 +0800

    add get-my-table

--- src/mock/mockjs.js
+++ src/mock/mockjs.js
@@ -9,7 +9,7 @@ const produceData = function (opt) {
     let newArticleObject = {
       title: Random.csentence(5, 30), // Random.csentence( min, max )
       thumbnail_pic_s: Random.dataImage('300x250', 'mock的图片'), // Random.dataImage( size, text ) 生成一段随机的 Base64 图片编码
-      author_name: Random.cname(), // Random.cname() 随机生成一个常见的中文姓名
+      author_name: Random.name(), // Random.cname() 随机生成一个常见的中文姓名
       date: Random.date() + ' ' + Random.time() // Random.date()指示生成的日期字符串的格式,默认为yyyy-MM-dd；Random.time() 返回一个随机的时间字符串
     }
     articles.push(newArticleObject)
